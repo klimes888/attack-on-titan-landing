23a45b53eaed85f504691fbedf7a66d52b3ac57e
klimes888 <klimes888@gmail.com>
2025-06-05 15:12:31 +0900

    add img list

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,24 @@ import Header from "@/components/header";
 import HeroSection from "@/components/hero-section";
 import PopularClips from "@/components/popular-clips";
 import "./globals.css";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function AttackOnTitanLanding() {
   const [isLoaded, setIsLoaded] = useState(false);
-
+  const isMobile = useIsMobile();
   useEffect(() => {
     setIsLoaded(true);
   }, []);
 
   return (
     <div className={`landing-container ${isLoaded ? "loaded" : ""}`}>
-      <div className="background-overlay" />
-      <div className="animated-particles animated-1" />
-      <div className="animated-particles animated-2" />
+      {!isMobile && (
+        <>
+          <div className="background-overlay" />
+          <div className="animated-particles animated-1" />
+          <div className="animated-particles animated-2" />
+        </>
+      )}
       <Header />
       <HeroSection />
       <PopularClips />
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,32 +1,36 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Image, { StaticImageData } from "next/image";
 
-import Levi from "@/assets/Levi.png";
-import Back from "@/assets/Back.jpeg";
+import Levi2 from "@/assets/Levi2.webp";
+import Eren from "@/assets/Eren.webp";
+import Mikasa from "@/assets/Mikasa.webp";
+import Armin from "@/assets/Armin.webp";
+import Back from "@/assets/Back.webp";
 
 export default function HeroSection() {
-  const [animations, setAnimations] = useState({
-    text: false,
-    character: false,
-  });
+  const [current, setCurrent] = useState(0);
+
+  const soptlight = [
+    { img: Eren, name: ["EREN", "YEAGER"] },
+    // { img: Mikasa, name: ["MIKASA", "ACKERMAN"] },
+    // { img: Armin, name: ["ARMIN", "ARLERT"] },
+    { img: Levi2, name: ["LEVI", "ACKERMAN"] },
+  ];
 
   useEffect(() => {
-    const timer1 = setTimeout(
-      () => setAnimations((prev) => ({ ...prev, text: true })),
-      500
-    );
-    const timer2 = setTimeout(
-      () => setAnimations((prev) => ({ ...prev, character: true })),
-      1000
-    );
+    const enterDuration = 4000;
+    const exitDuration = 1500;
+
+    const switchTimer = setTimeout(() => {
+      setCurrent((prev) => (prev + 1) % soptlight.length);
+    }, enterDuration + exitDuration);
 
     return () => {
-      clearTimeout(timer1);
-      clearTimeout(timer2);
+      clearTimeout(switchTimer);
     };
-  }, []);
+  }, [current]);
 
   const imageRender = ({
     img,
@@ -38,38 +42,52 @@ export default function HeroSection() {
     isBlur?: boolean;
   }) => {
     return (
+      // <div className="character-image"></div>
       <Image
         src={img}
         alt="Eren Jaeger"
         width={size.w}
         height={size.h}
-        className={`character-image ${isBlur && "img-blur"}`}
+        className={`character-image ${current === 0 && "img-eren"}`}
+        unoptimized
       />
     );
   };
 
   return (
     <section className="hero-section">
-      {/* <div className="hide-container" /> */}
       <div className="hero-content">
         <div className="hero-text-container">
-          <h1 className={`hero-title ${animations.text ? "visible" : ""}`}>
-            <span className="title-line">LEVI</span>
-            <span className="title-line">ACKERMAN</span>
-          </h1>
-          <p className={`hero-subtitle ${animations.text ? "visible" : ""}`}>
-            進撃の巨人
-          </p>
+          {soptlight.map(({ name }, index) => {
+            if (index !== current) return null;
+            return (
+              <div key={index} className="text-container-inner">
+                <div className="hero-title">
+                  <span className="title-line">{name[0]}</span>
+                  <span className="title-line">{name[1]}</span>
+                </div>
+                <p className="hero-subtitle">進撃の巨人</p>
+              </div>
+            );
+          })}
         </div>
-
-        {/* <div className="character-glow" /> */}
-        <div
-          className={`hero-character ${animations.character ? "visible" : ""}`}
-        >
-          {imageRender({ img: Levi, size: { w: 600, h: 800 }, isBlur: true })}
-          {imageRender({ img: Levi, size: { w: 300, h: 500 } })}
-          <div className="character-glow img-blur" />
-        </div>
+        <div className="hero-img-container"></div>
+        {soptlight.map(({ img }, index) => {
+          if (index !== current) return null;
+          return (
+            <div key={index} className="hero-character">
+              <div className="character-image-wrap img-blur">
+                {imageRender({
+                  img,
+                  size: { w: 600, h: 800 },
+                })}
+              </div>
+              <div className={`character-image-wrap`}>
+                {imageRender({ img, size: { w: 400, h: 600 } })}
+              </div>
+            </div>
+          );
+        })}
       </div>
 
       <div className="floating-elements">
@@ -82,9 +100,10 @@ export default function HeroSection() {
         <Image
           src={Back}
           alt="background"
-          width={1280}
-          height={768}
           className="background-image"
+          width={1280}
+          height={960}
+          unoptimized
         />
         <div className="background-image-blur" />
       </div>
